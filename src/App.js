9373e8848fcedcfb7cb01b5f9f629d7d9792c000
LeVzh <ion_king_zh9@mail.ru>
2018-11-04 12:55:19 +0300

    make carousel funkcional

--- src/App.js
+++ src/App.js
@@ -8,75 +8,93 @@ import Navbar from './Components/Navbar';
 const cakesArr= [
   {
     id: 1,
-    ref: './cakes/1.jpg'
+    ref: './cakes/1.jpg',
+    legend: 'Vilka-huilka'
   },
   {
     id: 2,
-    ref: './cakes/2.jpg'
+    ref: './cakes/2.jpg',
+    legend: 'Vilka-huilka'
   },
   {
     id: 3,
-    ref: './cakes/3.jpg'
+    ref: './cakes/3.jpg',
+    legend: 'Vilka-huilka'
   },
   {
     id: 4,
-    ref: './cakes/4.jpg'
+    ref: './cakes/4.jpg',
+    legend: 'Vilka-huilka'
   },
   {
     id: 5,
-    ref: './cakes/5.jpg'
+    ref: './cakes/5.jpg',
+    legend: 'Vilka-huilka'
   },
   {
     id: 6,
-    ref: './cakes/6.jpg'
+    ref: './cakes/6.jpg',
+    legend: 'Vilka-huilka'
   },
   {
     id: 7,
-    ref: './cakes/7.jpg'
+    ref: './cakes/7.jpg',
+    legend: 'Vilka-huilka'
   },
   {
     id: 8,
-    ref: './cakes/8.jpg'
+    ref: './cakes/8.jpg',
+    legend: 'Vilka-huilka'
   },
   {
     id: 9,
-    ref: './cakes/9.jpg'
+    ref: './cakes/9.jpg',
+    legend: 'Vilka-huilka'
   },
   {
     id: 10,
-    ref: './cakes/10.jpg'
+    ref: './cakes/10.jpg',
+    legend: 'Vilka-huilka'
   },
   {
     id: 11,
-    ref: './cakes/11.jpg'
+    ref: './cakes/11.jpg',
+    legend: 'Vilka-huilka'
   },
   {
     id: 12,
-    ref: './cakes/12.jpg'
+    ref: './cakes/12.jpg',
+    legend: 'Vilka-huilka'
   },
   {
     id: 13,
-    ref: './cakes/13.jpg'
+    ref: './cakes/13.jpg',
+    legend: 'Vilka-huilka'
   },
   {
     id: 14,
-    ref: './cakes/14.jpg'
+    ref: './cakes/14.jpg',
+    legend: 'Vilka-huilka'
   },
   {
     id: 15,
-    ref: './cakes/15.jpg'
+    ref: './cakes/15.jpg',
+    legend: 'Vilka-huilka'
   },
   {
     id: 16,
-    ref: './cakes/16.jpg'
+    ref: './cakes/16.jpg',
+    legend: 'Vilka-huilka'
   },
   {
     id: 17,
-    ref: './cakes/17.jpg'
+    ref: './cakes/17.jpg',
+    legend: 'Vilka-huilka'
   },
   {
     id: 18,
-    ref: './cakes/18.jpg'
+    ref: './cakes/18.jpg',
+    legend: 'Vilka-huilka'
   },
 ]
 
--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -8,9 +8,10 @@ class DCarousel extends Component {
     renderCakes = () =>{
         const {data} = this.props
         let cakesImgs = null
-        cakesImgs = data.map((imgRef, index) => {
+        cakesImgs = data.map((imgRef) => {
             return  <div className="box">
                         <img className="cake-imgs" key={imgRef.id} src={imgRef.ref} />
+                        <p className="legend">{imgRef.legend}</p>
                     </div>
           })
     
@@ -20,7 +21,8 @@ class DCarousel extends Component {
     render() {
         return (
             
-            <Carousel showThumbs={false} autoPlay={false} centerMode={true}>
+            <Carousel showThumbs={false} autoPlay={true} centerMode={true} centerSlidePercentage={34} showStatus={false}
+            showIndicators={false} width="753px" stopOnHover={true}> 
                 {this.renderCakes()}
             </Carousel>
         );
