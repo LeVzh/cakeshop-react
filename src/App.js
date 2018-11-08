794495c1f80f218d5488d3c8205e2e51bce4067e
LeVzh <ion_king_zh9@mail.ru>
2018-11-08 13:50:16 +0300

    intrgrate firebase in app, create form menu (application is working)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+
 import Header from './Components/Header';
 import Main from './Components/Main';
 import Navbar from './Components/Navbar';
+
+
 
 const cakesArr= [
   {
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,17 +1,68 @@
 import React, { Component } from 'react';
 
+import Collapsible from 'react-collapsible';
+
+
+
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
 import './css/main.css';
 
+var uuid = require('uuid');
+var firebase = require('firebase');
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDWhiS6XGQ1XcC3Qg4PkHbFgQaWVecKjv4",
+    authDomain: "cakeshoplistenner.firebaseapp.com",
+    databaseURL: "https://cakeshoplistenner.firebaseio.com",
+    projectId: "cakeshoplistenner",
+    storageBucket: "cakeshoplistenner.appspot.com",
+    messagingSenderId: "599979021552"
+  };
+  firebase.initializeApp(config);
+
 class Main extends Component {
 
   constructor(props) {
     super(props)
+    this.state = {
+      id: uuid.v1(),
+      name: '',
+      email: '',
+      comments: '',
+      submitted: false
+    }
+    this.handleChangeName = this.handleChangeName.bind(this)
+    this.handleChangeEmail = this.handleChangeEmail.bind(this)
+    this.handleChangeComments = this.handleChangeComments.bind(this)
   }
 
+  handleChangeName(e) {
+    this.setState({name: e.target.value})
+  }
+  handleChangeEmail(e) {
+    this.setState({email: e.target.value})
+  }
+  handleChangeComments(e) {
+    this.setState({comments: e.target.value})
+  }
+
+  handleSubmit(e) {
+    firebase.database().ref('users/'+this.state.id).set({
+      name: this.state.name,
+      email: this.state.email,
+      comment: this.state.comments
+    })
+    this.setState({
+      submitted: true
+    })
+    e.preventDefault();
+  }
+  
   getSnapshotBeforeUpdate() {
     if(this.props.currentCategory === 'married')
       this.slider.slickGoTo(0)
@@ -26,13 +77,14 @@ class Main extends Component {
     if(this.props.currentCategory === 'other')
       this.slider.slickGoTo(15)
   }
+  componentDidUpdate(){}
 
   renderCakes = () =>{
     const {data} = this.props
     let cakesImgs = null
     cakesImgs = data.map((imgRef) => {
-        return  <div className="box">
-                    <img className="cake-imgs" key={imgRef.id} src={imgRef.ref} />
+        return  <div key={imgRef.id} className="box">
+                    <img className="cake-imgs"  src={imgRef.ref} />
                 </div>
       })
     return cakesImgs;
@@ -40,6 +92,25 @@ class Main extends Component {
   
 
   render() {
+    
+    var order = null
+    if(!this.state.submitted) {
+      order = <Collapsible trigger="Сделать заказ" >
+                <form onSubmit={this.handleSubmit.bind(this)}>
+                  <input type="text" value={this.state.name} onChange={this.handleChangeName} placeholder="Введите имя"></input><br/>
+                  <input type="text" value={this.state.email} onChange={this.handleChangeEmail} placeholder="Введите email"></input><br/>
+                  <input type="text" value={this.state.comments} onChange={this.handleChangeComments} placeholder="Введите комментарий"></input><br/>
+                  <input type="submit" className='buttnSubmit'value="Отправить" />
+                </form>
+              </Collapsible> 
+    } else {
+      order = <div className="elem">
+                <h2>Спасибо, {this.state.name}.</h2>
+                <p>Мы обязательно свяжемся с вами.</p>
+              </div>
+    }
+      
+
     const settings = {
       autoplay: false,
       arrows: false,
@@ -53,11 +124,17 @@ class Main extends Component {
     
     return (
       <div className="main">
-        <div className="post-header-m"> 
-        <h1 className='main-title'>Мои работы</h1>           
+        <div className="post-header-m">
+        {order}
         <Slider ref={c => (this.slider = c)} {...settings}>
           {this.renderCakes()}
-        </Slider> 
+        </Slider>
+        <p className="elem">Здесь записана вся возможная информация о наших тортах. 
+          И прочее, что автор хотел бы сюда добавить. Так же сдесь можно разметить и другую информация, 
+          которую не дали в шапке. Большое спасибо за внимание!</p>
+          <p className="elem">Здесь записана вся возможная информация о наших тортах. 
+          И прочее, что автор хотел бы сюда добавить. Так же сдесь можно разметить и другую информация, 
+          которую не дали в шапке. Большое спасибо за внимание!</p>
           <div className="border" >
             <span></span>
             <span></span>
